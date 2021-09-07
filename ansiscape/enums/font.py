741fb111831c4c7b831dfee0e7d988b7bd46be45
Cariad Eccleston <cariad@cariad.io>
2021-09-07 14:39:29 +0100

    Renamed FontFace to Font

---
+++ ansiscape/enums/font.py
@@ -0,0 +1,15 @@
+from enum import IntEnum, unique
+
+
+@unique
+class Font(IntEnum):
+    DEFAULT = 0
+    ALT_0 = 1
+    ALT_1 = 2
+    ALT_2 = 3
+    ALT_3 = 4
+    ALT_4 = 5
+    ALT_5 = 6
+    ALT_6 = 7
+    ALT_7 = 8
+    ALT_8 = 9
