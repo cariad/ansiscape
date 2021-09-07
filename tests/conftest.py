10ed399049edcf2821726fb8b6c4cc6d2246023b
Cariad Eccleston <cariad@cariad.io>
2021-09-07 14:48:37 +0100

    Delete old tests

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-from pytest import fixture
-
-from ansiscape import InterpretationDict, make_interpretation
-
-
-@fixture
-def interpretation() -> InterpretationDict:
-    return make_interpretation()
