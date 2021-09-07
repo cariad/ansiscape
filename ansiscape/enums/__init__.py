3a4c1cb4b7af005275e2a955ad6b992a4df6bdc0
Cariad Eccleston <cariad@cariad.io>
2021-09-07 14:41:12 +0100

    Add InterpretationKey and SelectGraphicRendition

--- ansiscape/enums/__init__.py
+++ ansiscape/enums/__init__.py
@@ -1,21 +1,23 @@
 from ansiscape.enums.blink_speed import BlinkSpeed
-from ansiscape.enums.color_type import ColorType
-from ansiscape.enums.font_face import FontFace
+from ansiscape.enums.font import Font
 from ansiscape.enums.frame import Frame
 from ansiscape.enums.ideogram import Ideogram
-from ansiscape.enums.intensity import Intensity
+from ansiscape.enums.interpretation_key import InterpretationKey
+from ansiscape.enums.select_graphic_rendition import SelectGraphicRendition
 from ansiscape.enums.standard_color import StandardColor
 from ansiscape.enums.underline import Underline
 from ansiscape.enums.vertical_position import VerticalPosition
+from ansiscape.enums.weight import Weight
 
 __all__ = [
     "BlinkSpeed",
-    "ColorType",
-    "FontFace",
+    "Font",
     "Frame",
     "Ideogram",
-    "Intensity",
+    "InterpretationKey",
+    "SelectGraphicRendition",
     "StandardColor",
     "Underline",
     "VerticalPosition",
+    "Weight",
 ]
