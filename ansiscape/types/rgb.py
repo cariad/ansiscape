bc14cd10bea11a87d55e438ba90c50bb2ef02f23
Christopher Trudeau <ctrudeau@arsensa.com>
2026-03-23 03:51:36 -0400

    Convert RGB and RGBA to NamedTuple classes (#53)

--- ansiscape/types/rgb.py
+++ ansiscape/types/rgb.py
@@ -1,5 +1,8 @@
-from typing import Tuple
+from typing import NamedTuple
 
 # Represents the red, green and blue (RGB) aspects of a colour. Each component
 # is described by a float from 0.0 to 1.0.
-RGB = Tuple[float, float, float]
+class RGB(NamedTuple):
+    red: float
+    green: float
+    blue: float
--- ansiscape/types/rgba.py
+++ ansiscape/types/rgba.py
@@ -1,7 +1,9 @@
-from typing import Tuple
-
-# ALPHA = Union[Literal[0], Literal[1]]
+from typing import NamedTuple
 
 # Represents the red, green, blue and alpha aspects of a colour. Each component
 # is described by a float from 0.0 to 1.0.
-RGBA = Tuple[float, float, float, float]
+class RGBA(NamedTuple):
+    red: float
+    green: float
+    blue: float
+    alpha: float
